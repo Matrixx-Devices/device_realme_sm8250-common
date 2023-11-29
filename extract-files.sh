6c67517b7106b58abbdb66d055f5441c2910c6c3
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2023-11-29 16:43:55 +0100

    sm8250-common: Link dolby blobs against v33 libstagefright_foundation
     * fixes crashes and absurdly high CPU usage while using dolby
    
    Co-authored-by: Woomymy <woomy@woomy.be>
    Change-Id: Ib72b775dae677f3085d0b0e2afcbb709d8690063
    Signed-off-by: mukesh22584 <mks22584@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -114,6 +114,9 @@ function blob_fixup() {
         vendor/bin/sensors.qti)
             "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite-3.9.1.so" "libprotobuf-cpp-full-3.9.1.so" "${2}"
             ;;
+        odm/lib/libdlbdsservice_v3_6.so | odm/lib/libstagefright_soft_ddpdec.so | odm/lib/libstagefrightdolby.so | odm/lib64/libdlbdsservice_v3_6.so)
+            "${PATCHELF}" --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${2}"
+            ;;
     esac
 }
 
