5e2340c6d10973e4d0b06bf6b51223643fa26dd2
aamirr-ali <amirwide@gmail.com>
2024-05-28 22:25:57 +0530

    sm8250-common: Build Dolby/ViperX

--- common.mk
+++ common.mk
@@ -23,6 +23,12 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # Inherit proprietary libraries
 $(call inherit-product, vendor/realme/sm8250-common/sm8250-common-vendor.mk)
 
+# Viper
+$(call inherit-product-if-exists, packages/apps/ViPER4AndroidFX/config.mk)
+
+#Dolby
+$(call inherit-product-if-exists, hardware/dolby/dolby.mk)
+
 # ANT
 PRODUCT_PACKAGES += \
     com.dsi.ant@1.0.vendor
