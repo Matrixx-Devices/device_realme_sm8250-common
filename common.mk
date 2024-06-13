de68a1b293b49ea8c2b2bb7d3d12c4195e04dcda
Chirayu Desai <chirayudesai1@gmail.com>
2024-06-13 21:10:42 +0530

    sm8250-common: Don't set BOARD_API_LEVEL
    build/make/core/board_config.mk:1001: error: BOARD_API_LEVEL must not
    set manully. The build system automatically sets this value..
    
    Change-Id: If59ae9dea6cb13701d7217a1c0a355d4e07113eb
    Signed-off-by: mukesh22584 <mks22584@gmail.com>

--- common.mk
+++ common.mk
@@ -414,6 +414,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.stepcounter.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepcounter.xml \
     frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepdetector.xml
 
+# Shipping API
+BOARD_SHIPPING_API_LEVEL := 30
+PRODUCT_SHIPPING_API_LEVEL := $(BOARD_SHIPPING_API_LEVEL)
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     device/realme/sm8250-common \
